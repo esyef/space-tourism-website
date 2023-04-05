269b5e309437175fd2d7e82f53d8ded2ef3a1db2
yefweb <yeferson.dev@gmail.com>
2023-04-05 12:57:25 -0500

    style(Eslint): setup eslint for cscode and script  run lintter

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      black: '#0B0D17',
+      periwinkleBlue: '#D0D6F9',
+      white: '#FFFF',
+      transparent: 'transparent',
+      current: 'currentColor'
+    },
+    fontFamily: {
+      barlow: ['Barlow Condensed', 'sans-serif'],
+      bellefair: ['Bellefair', 'serif']
+    },
+    fontSize: {
+      sm: ['14px'],
+      base: ['16px', '28px'],
+      lg: ['32px', '56px'],
+      xl: ['100px', '150px']
+    }
   },
   plugins: []
 }
