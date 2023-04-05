588cd8c85ca29e20b6c4b16fb074976c1bfbb918
yefweb <yeferson.dev@gmail.com>
2023-04-05 14:53:41 -0500

    feat: add folder assets

---
+++ src/assets/images.js
@@ -0,0 +1,9 @@
+import bgHomeMobile from './home/background-home-mobile.jpg'
+import bgHomeTablet from './home/background-home-tablet.jpg'
+import bgHomeDesktop from './home/background-home-desktop.jpg'
+
+export {
+  bgHomeMobile,
+  bgHomeTablet,
+  bgHomeDesktop
+}
