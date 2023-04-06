fc2dac6b50c13ca640088bc4e95993bbe4f910b3
yefweb <yeferson.dev@gmail.com>
2023-04-05 19:53:50 -0500

    chore: add export images by page destination

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,5 +1,15 @@
 import bgHomeMobile from './home/background-home-mobile.jpg'
 import bgHomeTablet from './home/background-home-tablet.jpg'
 import bgHomeDesktop from './home/background-home-desktop.jpg'
+import bgDestMobile from './destination/background-destination-mobile.jpg'
+import bgDestTablet from './destination/background-destination-tablet.jpg'
+import bgDestDesk from './destination/background-destination-desktop.jpg'
 
-export { bgHomeMobile, bgHomeTablet, bgHomeDesktop }
+export {
+  bgHomeMobile,
+  bgHomeTablet,
+  bgHomeDesktop,
+  bgDestMobile,
+  bgDestTablet,
+  bgDestDesk,
+}
