1b9e3647ee089ce7469ad9a495b34b939601a68c
yefweb <yeferson.dev@gmail.com>
2023-04-05 19:11:56 -0500

    feat: add button main and styles fot button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,18 @@ const Home = () => {
     <main className='bg-home h-screen grid place-content-center'>
       <h2 className='body text-periwinkleBlue'>SO, YOU WANT TO TRAVEL TO</h2>
       <h1 className='text-[80px] text-white font-bellefair'>SPACE</h1>
-      <p className="body text-white"> 
-        Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+      <p className='body text-white'>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
       </p>
+
+      <div>
+        <a href='#' className='button'>
+          EXPLORE
+        </a>
+      </div>
     </main>
   )
 }
